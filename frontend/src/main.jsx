312a862204194c5f010c881490e5d98559732e84
Angélina Droz <angieso91@gmail.com>
2024-07-15 15:50:51 +0200

    creating pages home and family

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App";
+import Home from "./pages/Home"
+import Family from "./pages/Family";
+
+
+
+const router = createBrowserRouter([
+  {
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/family",
+          element: <Family />,
+        },
+        ]
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+        <React.StrictMode>
+          <RouterProvider router={router} />
+        </React.StrictMode>
+);
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+
+export default function Home (){
+    return (
+        <>
+        <p>heloooo</p>
+        </>
+    );
+}
