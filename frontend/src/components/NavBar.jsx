24d014b415e5c6b22f17cc2eab01aa33c9345df4
Angélina Droz <angieso91@gmail.com>
2024-07-15 22:52:51 +0200

    navbar page : import logo and add link and button to log

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,6 +1,15 @@
-export default function Navbar() {
+import Logo from "../assets/pictures/logo.png"
+import { Link } from "react-router-dom"
+
+export default function Navbar(isLoggedIn) {
     return(
         <>
+        <img className="logo" src={Logo} alt="Logo : le message des signes et des mots est entouré de jouets" />
+
+
+       {isLoggedIn === true || undefined ? <Link to="">Se connecter</Link> : <button>se déconnecter</button> }
+       
+        
         </>
     );
-}
+} 
