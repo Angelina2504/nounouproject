6465d2a5111eba7ed33ede1efe85696fd56587bc
Angélina Droz <angieso91@gmail.com>
2024-07-15 16:47:44 +0200

    Link footer whithout Css

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom"
+
 export default function Footer () {
     return (
         <>
+            <Link href="https://www.pajemploi.urssaf.fr/pajewebinfo/cms/sites/pajewebinfo/accueil.html">Pajemploi - Urssaf</Link>
+            <Link href="https://www.nounou-top.fr/">Nounou-top</Link>
+            <Link href="https://assmat.loire-atlantique.fr/jcms/accueil-assistante-maternelle-fr-local_56369">Assmat-Loire-atlantique</Link>
         </>
     );
 }
