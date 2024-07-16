41566a985209fa623a590af3e1702c29f5469fbc
Angélina Droz <angieso91@gmail.com>
2024-07-16 11:14:58 +0200

    first steps navbar css

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,15 +1,17 @@
 import Logo from "../assets/pictures/logo.png"
 import { Link } from "react-router-dom"
 
+import "../styles/navbar.css"
+
 export default function Navbar(isLoggedIn) {
     return(
-        <>
+        <section className="navbar">
         <img className="logo" src={Logo} alt="Logo : le message des signes et des mots est entouré de jouets" />
 
 
-       {isLoggedIn === true || undefined ? <Link to="">Se connecter</Link> : <button>se déconnecter</button> }
+       {isLoggedIn === true || undefined ? <Link to="">Se connecter</Link> : <button className="deconexion">se déconnecter</button> }
        
         
-        </>
+        </section>
     );
 } 
