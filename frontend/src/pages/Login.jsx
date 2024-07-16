c8e2fbe27e618b1b50497ca85d68ef56edf923bb
Angélina Droz <angieso91@gmail.com>
2024-07-16 12:24:42 +0200

    All login page with draft css

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,19 +1,21 @@
+import "../styles/login.css"
+
 export default function Login() {
     return (
-        <>
+        <section className="login">
         <h2>Se connecter</h2>
-        
-        <form action="">
 
-            <label htmlFor="">Prénom de l'enfant</label>
+        <form className="formLogin" action="">
+
+            <label className="nameChild" htmlFor="">Prénom de l'enfant</label>
             <input type="text" required/>
 
-            <label htmlFor="">Mot de passe</label>
+            <label className="" htmlFor="">Mot de passe</label>
             <input type="text" required/>
 
             <button>Se connecter</button>
         </form>
         
-        </>
+        </section>
     )
 }
