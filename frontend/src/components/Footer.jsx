37d69e1da22f97909a87ed2922068ccb5d711f3b
Angélina Droz <angieso91@gmail.com>
2024-07-17 12:04:34 +0200

    link footer ok with target blank

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,9 +4,9 @@ import "../styles/footer.css"
 export default function Footer () {
     return (
         <div className="allLink">
-            <Link href="https://www.pajemploi.urssaf.fr/pajewebinfo/cms/sites/pajewebinfo/accueil.html">Pajemploi - Urssaf</Link>
-            <Link href="https://www.nounou-top.fr/">Nounou-top</Link>
-            <Link href="https://assmat.loire-atlantique.fr/jcms/accueil-assistante-maternelle-fr-local_56369">Assmat-Loire-atlantique</Link>
+            <Link target="_blank" to="https://www.pajemploi.urssaf.fr/pajewebinfo/cms/sites/pajewebinfo/accueil.html">Pajemploi - Urssaf</Link>
+            <Link target="_blank" to="https://www.nounou-top.fr/">Nounou-top</Link>
+            <Link target="_blank"to="https://assmat.loire-atlantique.fr/jcms/accueil-assistante-maternelle-fr-local_56369">Assmat-Loire-atlantique</Link>
         </div>
     );
 }
