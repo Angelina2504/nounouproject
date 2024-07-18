c6e0adaba85a0fb9d63aff8889188795c56813ca
Angélina Droz <angieso91@gmail.com>
2024-07-18 23:39:47 +0200

    creating intializeDb.js

---
+++ backend/database/initializeDatabase.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+const pool = require("./db-connection");
+
+const initializeDatabase = async () => {
+  try {
+    const sqlPath = path.join(__dirname, "datas.sql");
+    const sql = fs.readFileSync(sqlPath, "utf-8");
+
+    const queries = sql
+      .split(";")
+      .map((query) => query.trim())
+      .filter((query) => query.length);
+
+    const connection = await pool.getConnection();
+    for (const query of queries) {
+      await connection.query(query);
+    }
+    connection.release();
+
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.error("Error initializing database:", error);
+  }
+};
+
+module.exports = { initializeDatabase };
