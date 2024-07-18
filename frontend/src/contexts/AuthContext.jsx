d2fde3ae43e025ac94da6ca1c5136cc930a91103
Angélina Droz <angieso91@gmail.com>
2024-07-18 23:22:29 +0200

    in AuthContext add useEffect localStorage

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,11 +1,11 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 
 const AuthContext = createContext(null);
 
 export const useAuth = () => useContext(AuthContext);
 
 export function AuthProvider ({children}) {
-    const[auth, setAuth] = useState({name: "toto"});
+    const[auth, setAuth] = useState(null);
 
     function login(userData){
         setAuth(userData);
@@ -16,6 +16,19 @@ export function AuthProvider ({children}) {
     
     }
 
+    useEffect(()=> {
+        const storedAuth = localStorage.getItem("auth");
+        if (storedAuth) {
+            setAuth(JSON.parse(storedAuth)); }
+    }, [])
+
+    useEffect(() =>{
+     if (auth)  {
+        localStorage.setItem("auth", JSON.stringify(auth));}
+    else{
+        localStorage.removeItem("auth");
+    }
+    }, [auth]);
 
 return (
 <AuthContext.Provider value={{auth, login, logout}}>
