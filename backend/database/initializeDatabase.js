4469b0ab921830ebf55fd23a80767f841cb2c11a
Angélina Droz <angieso91@gmail.com>
2024-07-19 00:11:56 +0200

    creating routes files

--- backend/database/initializeDatabase.js
+++ backend/database/initializeDatabase.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const pool = require("./db-connection");
+const pool = require("./db_connection");
 
 const initializeDatabase = async () => {
   try {
---
+++ backend/routes/authroutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { login, register } = require("../controllers/authController");
+const {
+  checkLoginDatas,
+  checkRegisterDatas,
+} = require("../services/checkDatas");
+
+const router = express.Router();
+
+router.post("/login", checkLoginDatas, login);
+router.post("/register", checkRegisterDatas, register);
+
+module.exports = router;
