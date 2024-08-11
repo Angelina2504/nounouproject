f9a9f9a766daf67d34d03b27cfed5486a0b32bf8
Angie Perso <adroz.perso@gmail.com>
2024-08-11 21:44:39 +0200

    fix CRUD for tutorRepository

--- backend/models/tutorRepository.js
+++ backend/models/tutorRepository.js
@@ -1,57 +1,104 @@
 const AbstractRepository = require("./AbstractRepository");
 
 class TutorRepository extends AbstractRepository {
-  constructor() {
-    super({ table: "tutor" });
-  }
+    constructor() {
+        super({ table: "tutor" });
+        this.tableChildAssociation = "tutor_child"; // for association table tutor_child
+    }
 
-  async create(tutor) {
-    const [result] = await this.database.query(
-      `insert into ${this.table} (firstname, lastname, phone_nomber, address) values (?, ?, ?, ?)`,
-      [tutor.firstname,
-        tutor.lastname, 
-        tutor.phone_number,
-        tutor.address]
-    );
+    async create(tutor, childId) {
 
-    return result.insertId;
-  }
+        // On doit faire 2 inset into à la suite, donc on s'assure que soit les 2 passent, soit aucun des 2 en encapsulant
+        // les query dans une transaction, et en n'oubliant pas de fermer la connexion à la fin
+        const connection = await this.databasePool.getConnection();
 
-  async read(id) {
-    const [rows] = await this.database.query(
-      `select * from ${this.table} where id = ?`,
-      [id]
-    );
+        try {
+            // On ouvre la transaction
+            await connection.beginTransaction();
 
-    return rows[0];
-  }
+            const [result] = await this.databasePool.query(
+                `insert into ${this.table}(firstname, lastname, email, phone_number, address, user_id)
+                 values (?, ?, ?, ?, ?, ?)`,
+                [
+                    tutor.firstname,
+                    tutor.lastname,
+                    tutor.email,
+                    tutor.phoneNumber,
+                    tutor.address,
+                    tutor.userId
+                ]
+            );
 
-  async readAll() {
-    const [rows] = await this.database.query(`select * from ${this.table}`);
+            if (result && result.insertId) {
+                await this.databasePool.query(
+                    `insert into ${this.tableAssociation}(tutor_id, child_id)
+                     values (?, ?)`,
+                    [
+                        result.insertId,
+                        childId
+                    ]
+                );
+            }
 
-    return rows;
-  }
+            // On commit la transaction
+            await connection.commit();
 
-  async update(tutor) {
-    const [result] = await this.database.query(
-      `update ${this.table} set firstname = ?, lastname = ?, phone_number = ?, address = ?  where id = ?`,
-      [tutor.firstname,
-        tutor.lastname, 
-        tutor.phone_number,
-        tutor.address]
-    );
+            return result.insertId;
+        } catch (error) {
+            // En cas d'erreur on rollback la transaction et on annule les changements
+            await connection.rollback();
+            console.error("Error creating tutor", error);
+            throw error;
+        } finally {
+            // On ferme la connexion, qu'il y ait une erreur ou non
+            connection.release();
+        }
+    }
 
-    return result.affectedRows;
-  }
+    async read(id) {
+        const [rows] = await this.databasePool.query(
+            `select * from ${this.table} 
+             where id = ?`,
+            [id]
+        );
 
-  async delete(id) {
-    const [result] = await this.database.query(
-      `delete from ${this.table} where id = ?`,
-      [id]
-    );
+        return rows[0];
+    }
 
-    return result.affectedRows;
-  }
+    async readAll() {
+        const [rows] = await this.databasePool.query(
+            `select * from ${this.table}`
+        );
+
+        return rows;
+    }
+
+    async update(tutor) {
+        const [result] = await this.databasePool.query(
+            `update ${this.table} 
+             set firstname = ?, lastname = ?, email = ?, phone_number = ?, address = ?, user_id = ?
+             where id = ?`,
+            [
+                tutor.firstname,
+                tutor.lastname,
+                tutor.email,
+                tutor.phoneNumber,
+                tutor.address,
+                tutor.userId
+            ]
+        );
+
+        return result.affectedRows;
+    }
+
+    async delete(id) {
+        const [result] = await this.databasePool.query(
+            `delete from ${this.table} where id = ?`,
+            [id]
+        );
+
+        return result.affectedRows;
+    }
 }
 
 module.exports = TutorRepository;
