14f37e7eae9f14ea0a3973538298b7b9920538e9
Angélina Droz <angieso91@gmail.com>
2024-09-11 17:41:34 +0200

    first step profil

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,44 @@
 const userRepository = require('../models/userRepository');
 
+// Browse all tutors
+const browse = async (req, res) => {
+    try {
+        // retrieve current user id from session 
+        const userId = req.session.user.id;
+
+        // Get all tutors from the database
+        const user = await userRepository.readAllForUser(userId);
+
+        // Respond with the tutors in JSON format
+        res.status(200).json({ success: true, user: user });
+    } catch (err) {
+        // In case of an error, log it and return an error response
+        console.error("Error during browsing user:", err);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+};
+
+// Get a tutor by its id
+const read = async (req, res) => {
+    try {
+
+        // Search tutor by id
+        const tutor = await userRepository.read(req.params.id);
+
+        // If the tutor is not found, return an error response
+        if (tutor == null) {
+            res.status(404).json({ success: false, message: "User not found" });
+        } else {
+            // In case of success, return the tutor in JSON format
+            res.status(200).json({ success: true, user: user });
+        }
+    } catch (err) {
+        // In case of an error, log it and return an error response
+        console.error("Error during reading user:", err);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+}
+
 /**
  * Update a user in the database.
  *
@@ -25,5 +64,7 @@ const edit = async (req, res) => {
 }
 
 module.exports = {
+    browse,
+    read,
     edit
 }
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,17 +1,25 @@
-import TutorFile from "../components/TutorFile";
-import ChildFile from "../components/ChildFile";
-import { useCheckConnected } from "../hooks/useCheckConnected";
-
+import { useState, useEffect } from "react";
+import ProfilList from "../components/ProfilList"
+import axiosInstance from "../services/httpClient";
 
 export default function Profile () {
 
-    const { user } = useCheckConnected();
+    const [users, setUser] = useState([]);
+
+    // // Fonction pour récupérer la liste des users
+    const fetchTutors = async () => {
+        try {
+            const response = await axiosInstance.get('/tutors');
+            setUser(response.data.tutors);
+        } catch (error) {
+            console.error('Erreur lors de la récupération des enfants', error);
+        }
+    };
 
     return (          
         <>
-            <p>Page du profil si connecté !</p>
-            {/*<TutorFile/>;*/}
-            {/*<ChildFile/>*/}
+            <ProfilList/>
+            
         </>
     );
  }
