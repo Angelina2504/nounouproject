39e653853598d53351ac56ad9a9cc44099ffcd4c
Angelina2504 <angieso91@gmail.com>
2025-07-09 22:07:31 +0200

    Netlify CORS cookie config

--- frontend/src/services/httpClient.js
+++ frontend/src/services/httpClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: `${import.meta.env.VITE_API_URL}` || '/api', // URL de l'API du back end
-    withCredentials: false // permet de passer les cookies
+    withCredentials: true // permet de passer les cookies
 });
 
 export default axiosInstance;
