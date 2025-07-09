b0e6d825165558768fb11f086b5ad3dd9b4b5979
Angelina2504 <angieso91@gmail.com>
2025-07-09 21:44:36 +0200

    config for production CORS/proxy

--- frontend/src/services/httpClient.js
+++ frontend/src/services/httpClient.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}`, // URL de l'API du back end
-    withCredentials: true // permet de passer les cookies
+    baseURL: `${import.meta.env.VITE_API_URL}` || '/api', // URL de l'API du back end
+    withCredentials: false // permet de passer les cookies
 });
 
 export default axiosInstance;
